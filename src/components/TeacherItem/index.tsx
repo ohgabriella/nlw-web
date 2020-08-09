0fa76d438e4e4ece7fc06e84978ddc275440ad1b
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2020-08-09 11:34:58 -0300

    relacionando as Connections

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 
 export interface Teacher {
@@ -20,6 +21,13 @@ interface TeacherItemṔrops {
 }
 
 const TeacherItem: React.FC<TeacherItemṔrops> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
+
     return (
         <article className="teacher-item">
             <header>
@@ -33,11 +41,19 @@ const TeacherItem: React.FC<TeacherItemṔrops> = ({ teacher }) => {
             <p>{teacher.bio}</p>
 
             <footer>
-                <p>Preço por hora <strong>R$ {teacher.cost}</strong></p>
-                <button type="button">
-                    <img src={whatsappIcon} alt="Whatsapp" />
+                <p>
+                    Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
+                <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
